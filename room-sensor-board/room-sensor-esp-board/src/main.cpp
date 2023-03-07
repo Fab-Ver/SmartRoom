517a6956cba8b4b9c75400ba76f847278fcb2622
Fabio Veroli <fabio.veroli@studio.unibo.it>
2023-03-07 12:45:38 +0100

    Create tasks functions

--- room-sensor-board/room-sensor-esp-board/src/main.cpp
+++ room-sensor-board/room-sensor-esp-board/src/main.cpp
@@ -1,9 +1,50 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <PubSubClient.h>
+#include "config.h"
+#include "Led.h"
+#include "LightSensor.h"
+#include "Pir.h"
+
+Light* led;
+PhotoResistor* ls;
+MotionSensor* pir;
+
+TaskHandle_t CommunicationHandle;
+TaskHandle_t LedHandle;
+
+void CommunicationTask(void* parameter);
+void LedTask(void* parameter);
+
+WiFiClient espClient;
+PubSubClient client(espClient);
 
 void setup() {
-  // put your setup code here, to run once:
+  led = new Led(LED_PIN);
+  ls = new LightSensor(LS_PIN);
+  pir = new Pir(PIR_PIN);
+  xTaskCreatePinnedToCore(CommunicationTask,"CommunicationTask",1000,NULL,1,&CommunicationHandle,0);
+  delay(500);
+  xTaskCreatePinnedToCore(LedTask,"LedTask",1000,NULL,1,&LedHandle,1);
+  delay(500);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+}
+
+void CommunicationTask(void* paramater){
+  for(;;){
+    
+  }
+}
+
+void LedTask(void* paramater){
+  for(;;){
+    if(pir->isDetected()){
+      led->switchOn();
+    } else {
+      led->switchOff();
+    } 
+  }
 }
