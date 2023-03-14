adec9a0fdf6767dc6382280857ee99471bcb4892
Fabio Veroli <fabio.veroli@studio.unibo.it>
2023-03-14 12:27:42 +0100

    Add object init in setup and implemented loop function

--- room-controller/room-controller/src/main.cpp
+++ room-controller/room-controller/src/main.cpp
@@ -1,9 +1,34 @@
 #include <Arduino.h>
+#include "config.h"
+#include "ModuleBT.h"
+#include "SerialCommunicator.h"
+#include "Led.h"
+#include "RollerBlind.h"
+
+ModuleBT* bt;
+SerialCommunicator* serial = new SerialCommunicator();
+Light* led;
+RollerBlind* rb;
 
 void setup() {
-  // put your setup code here, to run once:
+  bt = new ModuleBT(RX_PIN, TX_PIN);
+  bt->init();
+  serial->init();
+  led = new Led(LED_PIN);
+  rb = new RollerBlind(SERVO_PIN);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if(serial->isMsgAvailable()){
+    Msg* msg = serial->receiveMsg();
+    bt->sendMsg(Msg(msg->getContent()));
+    /*JSON e controlli*/
+    delete msg;
+  }
+
+  if(bt->isMsgAvailable()){
+    Msg* msg = bt->receiveMsg();
+    serial->sendMsg(Msg(msg->getContent()));
+    delete msg;
+  }
 }
