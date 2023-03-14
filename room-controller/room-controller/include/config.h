42cf322295bdd907c7cf56060b399d756a0b920e
Fabio Veroli <fabio.veroli@studio.unibo.it>
2023-03-14 12:25:35 +0100

    Add configuration file

---
+++ room-controller/room-controller/include/config.h
@@ -0,0 +1,6 @@
+/* RX is pin 2 => to be connected to TXD of BT module*/
+#define RX_PIN 2 
+/* TX is pin3 => to be connected to RXD of BT module*/
+#define TX_PIN 3
+#define LED_PIN 4
+#define SERVO_PIN 5
