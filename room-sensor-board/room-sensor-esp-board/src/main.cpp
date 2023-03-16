c215e90aa280ca1d9176074aa148f285667f3afd
Fabio Veroli <fabio.veroli@studio.unibo.it>
2023-03-16 09:45:58 +0100

    Implement recconect() function, send json data using mqtt

--- room-sensor-board/room-sensor-esp-board/src/main.cpp
+++ room-sensor-board/room-sensor-esp-board/src/main.cpp
@@ -5,6 +5,7 @@
 #include "Led.h"
 #include "LightSensor.h"
 #include "Pir.h"
+#include "ArduinoJson.h"
 
 Light* led;
 PhotoResistor* ls;
@@ -15,14 +16,24 @@ TaskHandle_t LedHandle;
 
 void CommunicationTask(void* parameter);
 void LedTask(void* parameter);
+void setup_wifi();
+void reconnect();
+
+/* MQTT subscribing callback */
+void callback(char* topic, byte* payload, unsigned int length);
 
 WiFiClient espClient;
 PubSubClient client(espClient);
+unsigned long lastMsg = 0; 
 
 void setup() {
   led = new Led(LED_PIN);
   ls = new LightSensor(LS_PIN);
   pir = new Pir(PIR_PIN);
+  setup_wifi();
+  randomSeed(micros());
+  client.setServer(mqtt_server, 1883);
+  client.setCallback(callback);
   xTaskCreatePinnedToCore(CommunicationTask,"CommunicationTask",1000,NULL,1,&CommunicationHandle,0);
   delay(500);
   xTaskCreatePinnedToCore(LedTask,"LedTask",1000,NULL,1,&LedHandle,1);
@@ -35,7 +46,20 @@ void loop() {
 
 void CommunicationTask(void* paramater){
   for(;;){
-    
+    if(!client.connected()){
+      reconnect();
+    }
+    client.loop();
+    unsigned long now = millis();
+    if(now - lastMsg >= MSG_PERIOD){
+      lastMsg = now;
+      DynamicJsonDocument doc(1024);
+      doc['detected'] = String(pir->isDetected());
+      doc['dark'] = String(ls->isDark());
+      char* msg_json;
+      serializeJson(doc,msg_json);
+      client.publish(topic,msg_json);
+    }
   }
 }
 
@@ -46,5 +70,46 @@ void LedTask(void* paramater){
     } else {
       led->switchOff();
     } 
+  }
+}
+
+void setup_wifi(){
+  delay(10);
+
+  Serial.println(String("Connecting to ") + ssid);
+
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid,password);
+
+  while(WiFi.status() != WL_CONNECTED){
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi connected");
+}
+
+void callback(char* topic, byte* payload, unsigned int length) {
+  Serial.println(String("Message arrived on [") + topic + "] len: " + length );
+}
+
+void reconnect(){
+  while(!client.connected()){
+    Serial.print("Attempting MQTT connection...");
+
+    /*Create a random client ID*/
+    String clientID = String("room-client-") + String(random(0xffff), HEX);
+
+    /*Attempt to connect*/
+    if(client.connect(clientID.c_str())){
+      Serial.println("connected");
+      client.subscribe(topic);
+    } else {
+      Serial.print("failed rc=");
+      Serial.print(client.state());
+      Serial.println(" try again in 5 seconds.");
+      delay(5000);
+    }
   }
 }
