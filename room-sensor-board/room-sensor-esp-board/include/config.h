ae624f738772a65ddacf634fc01965d0c4a2f537
Fabio Veroli <fabio.veroli@studio.unibo.it>
2023-03-16 09:47:51 +0100

    Add server constants to configuration file

--- room-sensor-board/room-sensor-esp-board/include/config.h
+++ room-sensor-board/room-sensor-esp-board/include/config.h
@@ -1,3 +1,21 @@
 #define LED_PIN 1
 #define PIR_PIN 2
 #define LS_PIN 3
+#define MSG_PERIOD 200
+#define MSG_BUFFER_SIZE 50
+
+/**
+ * WIFI network info
+*/
+const char* ssid = "nome";
+const char* password = "password";
+
+/**
+ * MQTT server address 
+*/
+const char* mqtt_server = "broker.mqtt-dashboard.com";
+
+/**
+ * MQTT topic
+*/
+const char* topic = "data";
