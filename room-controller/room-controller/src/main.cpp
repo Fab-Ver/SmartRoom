3d6ff91307605ecfa06c92b5faeb25962fa20e64
Fabio Veroli <fabio.veroli@studio.unibo.it>
2023-04-27 13:43:19 +0200

    Change communication system and add change state function

--- room-controller/room-controller/src/main.cpp
+++ room-controller/room-controller/src/main.cpp
@@ -1,50 +1,76 @@
 #include <Arduino.h>
 #include "config.h"
-#include "ModuleBT.h"
+#include "BluetoothCommunicator.h"
 #include "SerialCommunicator.h"
 #include "Led.h"
 #include "RollerBlind.h"
 #include "ArduinoJson.h"
 
-ModuleBT* bt;
-SerialCommunicator* serial = new SerialCommunicator();
+BluetoothCommunicator* bluetooth;
+SerialCommunicator* serial;
 Light* led;
-RollerBlind* rb;
+RollerBlind* rollerBlind;
+
+LightState prevLightState = UNDEFINED;
+int prevRollerBlindState = UNKNOWN;
+
+void modifyState(String msg_json);
 
 void setup() {
-  bt = new ModuleBT(RX_PIN, TX_PIN);
-  bt->init();
-  serial->init();
-  led = new Led(LED_PIN);
-  rb = new RollerBlind(SERVO_PIN);
+	serial = new SerialCommunicator();
+	bluetooth = new BluetoothCommunicator(RX_PIN, TX_PIN);
+	serial->init();
+	bluetooth->init();
+	led = new Led(LED_PIN);
+	rollerBlind = new RollerBlind(SERVO_PIN);
 }
 
 void loop() {
-  if(serial->isMsgAvailable()){
-    Msg* msg = serial->receiveMsg();
-    bt->sendMsg(Msg(msg->getContent()));
-
-    DynamicJsonDocument doc(1024);
-    deserializeJson(doc,msg->getContent());
-    String led_state = doc["led_state"];
-    int rb_angle = doc["rb_angle"];
-
-    if(led_state == "ON"){
-      led->switchOn();
-    } else {
-      led->switchOff();
-    }
-
-    rb->on();
-    rb->setPosition(rb_angle);
-    rb->off();
-
-    delete msg;
-  }
-
-  if(bt->isMsgAvailable()){
-    Msg* msg = bt->receiveMsg();
-    serial->sendMsg(Msg(msg->getContent()));
-    delete msg;
-  }
+	if(serial->isMsgAvailable()){
+    	Msg* msg = serial->receiveMsg();
+    	modifyState(msg->getContent());
+    	bluetooth->sendMsg(Msg(msg->getContent()));
+    	delete msg;
+  	}
+  	if(bluetooth->isMsgAvailable()){
+    	Msg* msg = bluetooth->receiveMsg();
+    	modifyState(msg->getContent());
+    	serial->sendMsg(Msg(msg->getContent()));
+    	delete msg;
+  	}
 }
+
+void modifyState(String msg_json){
+	DynamicJsonDocument doc(1024);
+	DeserializationError error = deserializeJson(doc, msg_json);
+	if(error){
+		Serial.print(F("deserializeJson() failed: "));
+    	Serial.println(error.f_str());
+    	return;
+	}
+	JsonObject json = doc.as<JsonObject>();
+	if(json.containsKey("light")){
+		bool light = doc["light"];
+		LightState currLightState = light ? ON : OFF;
+		if(currLightState != prevLightState){
+			if(currLightState == ON){
+				led->switchOn();
+			} else {
+				led->switchOff();
+			}
+			prevLightState = currLightState;
+		}
+	}
+	if(json.containsKey("roller_blind")){
+		int currRollerBlindState = doc["roller_blind"];
+		if(currRollerBlindState != prevRollerBlindState){
+			int angle = map(currRollerBlindState, 0, 100, 0, 180);
+			rollerBlind->on();
+			rollerBlind->setPosition(angle);
+			rollerBlind->off();
+			prevRollerBlindState = currRollerBlindState;
+		}
+
+	}
+}
+
